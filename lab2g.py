3a6d35d4018fa33f88ee6d718dd6ab84383922cc
Mohammad Iqbal <miqbal37@myseneca.ca>
2025-01-23 22:59:12 -0500

    added lab2g

---
+++ lab2g.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+#Author: Mohammad Iqbal
+#Author ID: 105830228
+#Date Created: 2024/09/18
+
+import sys
+
+if len(sys.argv) == 1:
+    timer = 3
+else:
+    timer = int(sys.argv[1])
+
+while timer > 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
