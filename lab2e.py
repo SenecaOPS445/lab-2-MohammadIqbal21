63273749c9f51f6256b9c367a73f9b5d9963be24
Mohammad Iqbal <miqbal37@myseneca.ca>
2025-01-23 22:39:37 -0500

    added lab 2e

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#Author: Mohammad Iqbal
+#Author ID: 105830228
+#Date Created: 2024/09/18
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
